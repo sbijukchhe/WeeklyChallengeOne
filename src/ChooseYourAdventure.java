b3c6e3f244577fe4efe636ecb7229de02aeef355
sbijukchhe <skbijukchhe@gmail.com>
2019-09-05 21:24:56 -0400

    First weekly challenge on nested switch statements

--- src/ChooseYourAdventure.java
+++ src/ChooseYourAdventure.java
@@ -1,3 +1,17 @@
+/*
+Make a "Choose Your Own Adventure" game. It should feature at least 5  options after the first level prompts,
+at least two options for each of the second-level prompts, at least two options for each of the third-level prompts,
+ and add at least two options for two of the fourth-level prompts for a total of a minimum of twenty two possible
+  destinations.  If you finish please add more options to the first level and follow the same structure.
+
+Once they reach the end of the decision tree, tell the user whether they survived or not. For example, in the first
+sample output below,
+
+You die of starvation... eventually
+statement informs the user that they did not survive the game.
+ */
+
+
 import java.util.Scanner;
 
 public class ChooseYourAdventure {
